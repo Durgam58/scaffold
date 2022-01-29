7ff428033dc300eaf14e5009e76b3fbe03f1d5d3
DUAY <durgam7058@gmail.com>
2022-01-29 20:22:30 +0100

    renamed file name

---
+++ test_hello.py
@@ -0,0 +1,14 @@
+from hello import add
+
+def test_add():
+    assert add(1, 2) == 3
+
+
+
+
+#import unittest
+#from hello import add
+
+#class TestHello(unittest.TestCase):
+#	def test_add(self):
+#		self.assertAlmostEqual(add(1,3), 4)
